adc341608d2c2a6a4ee89142d0a0c42df80e41cd
danmichaelo <danmichaelo@gmail.com>
2016-08-21 13:25:06 +0000

    Applied fixes from StyleCI

--- tests/InvalidRecordTest.php
+++ tests/InvalidRecordTest.php
@@ -4,9 +4,9 @@ use Scriptotek\Marc\Importers\XmlImporter;
 
 class InvalidRecordTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @expectedException Scriptotek\Marc\Exceptions\XmlException
-	 */
+    /**
+     * @expectedException Scriptotek\Marc\Exceptions\XmlException
+     */
     public function testAlmaBibsApiExample()
     {
         // Expect failure because of invalid encoding in XML declaration:
