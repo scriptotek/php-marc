d9e740b94a78e61449c3810c201b6f12a653be35
Dan Michael O. Heggø <Dan.Michael.Heggo@bibsent.no>
2022-12-17 21:07:15 +0100

    Add return type

--- src/Record.php
+++ src/Record.php
@@ -219,7 +219,7 @@ class Record implements JsonSerializable
      *
      * @return array
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         $o = [];
         foreach ($this->properties as $prop) {
